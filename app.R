a6e2b0978c3c8d15109b2ebcbb552ce4a3fe10b9
lukehorton1 <lukeowen02@gmail.com>
2024-11-06 16:46:38 +0000

    Fixed close price issue

--- app.R
+++ app.R
@@ -24,7 +24,7 @@ ui <- page_sidebar(
   layout_columns(
     fill = FALSE,
     value_box(
-      title = "Price",
+      title = "Previous Close",
       value = textOutput("price"),
       showcase = bsicons::bs_icon("cash")
     ),
@@ -56,7 +56,7 @@ server <- function(input, output) {
   
   # Note: current_data must be called with parenthesis as it is a function
   output$price <- renderText({
-    format(current_data()[["Open"]], big.mark = ",")
+    format(current_data()[["Close"]], big.mark = ",")
     })
   output$volume <- renderText({
     format(current_data()[["Volume"]], big.mark = ",")
